4ba4eee67a0a83a4b8da0672a8d0c942b9e3d08e
seokmin <seokmin0016@gmail.com>
2023-01-29 12:32:08 +0900

    중복된아이디 안되게 validation과 unique사용

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,13 @@
 import { AuthCredentialsDto } from './dto/auth-credential.dto';
 import { AuthService } from './auth.service';
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('/signup')
-  signUp(@Body() authCredentialsDto: AuthCredentialsDto) {
+  signUp(@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto) {
     return this.authService.signUp(authCredentialsDto);
   }
 }
--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -2,6 +2,10 @@ import { AuthCredentialsDto } from './dto/auth-credential.dto';
 import { CustomRepository } from 'src/typeorm-ex/typeorm-ex.decorator';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
+import {
+  ConflictException,
+  InternalServerErrorException,
+} from '@nestjs/common';
 
 @CustomRepository(User)
 export class UserRepository extends Repository<User> {
@@ -9,6 +13,14 @@ export class UserRepository extends Repository<User> {
     const { username, password } = authCredentialsDto;
     const user = this.create({ username, password });
 
-    await this.save(user);
+    try {
+      await this.save(user);
+    } catch (error) {
+      if (error.code === '23505') {
+        throw new ConflictException('이미있는아이디입니다');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 }
