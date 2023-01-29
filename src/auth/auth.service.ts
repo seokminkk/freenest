652c40215791735013a90bc3bc7fff3bd7489d9a
seokmin <seokmin0016@gmail.com>
2023-01-29 12:57:28 +0900

    bcript 로 서비스 로직구현

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,8 @@
 import { AuthCredentialsDto } from './dto/auth-credential.dto';
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserRepository } from './user.repository';
-
+import * as bcrypt from 'bcryptjs';
 @Injectable()
 export class AuthService {
   constructor(
@@ -11,5 +11,16 @@ export class AuthService {
 
   signUp(authCredentialsDto: AuthCredentialsDto) {
     return this.userRepository.createUser(authCredentialsDto);
+  }
+
+  async signIn(authCredentialsDto: AuthCredentialsDto) {
+    const { username, password } = authCredentialsDto;
+    const user = await this.userRepository.findOne({ where: { username } });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      return ` 로그인성공`;
+    } else {
+      throw new UnauthorizedException('로그인실패');
+    }
   }
 }
